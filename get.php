de329f2769ebea681a5d3a991445d9555de4f939
Parii <pm.aghazade@gmail.com>
2023-12-12 11:57:43 +0330

    Add files

---
+++ get.php
@@ -0,0 +1,29 @@
+﻿<?php 
+	include_once("sender.php");
+	$url = 'https://bitpay.ir/payment/gateway-result-second'; 
+	$api = 'Your-API';
+	$trans_id = $_GET['trans_id']; 
+	$id_get = $_GET['id_get'];
+	$result = get($url,$api,$trans_id,$id_get); 
+	
+	$parseDecode = json_decode($result);
+	
+	if($parseDecode->status == 1)
+	{
+		//true
+		
+		//mablagh ersali
+		echo $parseDecode->amount;
+		
+		//factore ersali (ekhtiari)
+		echo $parseDecode->factorId;
+		
+		//shomare kart pardakht konanade
+		echo $parseDecode->cardNum;
+		
+	}
+	else
+	{
+		//false
+	}
+?>
---
+++ send.php
@@ -0,0 +1,20 @@
+﻿<?php 
+	include_once("sender.php");
+	
+	$url = 'https://bitpay.ir/payment/gateway-send'; 
+	$api = 'Your-API';
+	$amount = 10000;
+	$redirect = 'REDIRECT-PAGE';
+	$name = '';//ekhtiari
+	$email = '';//ekhtiari
+	$description = '';//ekhtiari
+	$factorId = 1;//ekhtiari
+	
+	$result = send($url,$api,$amount,$redirect,$factorId,$name,$email,$description); 
+	
+	if($result > 0 && is_numeric($result))
+	{
+		$go = "https://bitpay.ir/payment/gateway-$result-get"; 
+		header("Location: $go");
+	} 
+?>
